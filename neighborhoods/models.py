98002f90774f779a7b8afdce4775119ae11163c4
brianmwevi <mwevibrian@gmail.com>
2022-06-16 13:28:45 +0300

    patch:model -- remove null and blank in location field

--- neighborhoods/models.py
+++ neighborhoods/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Neighborhood(models.Model):
     name = models.CharField(max_length=55)
-    location = models.CharField(max_length=55, blank=False, null=False)
+    location = models.CharField(max_length=55)
     occupants = models.IntegerField(default=1, blank=True, null=False)
     admin = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
